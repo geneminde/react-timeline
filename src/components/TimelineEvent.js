3c1f0050438c8131a797981e8f69295170263a82
Genevieve Neely <genevieve.hood@gmail.com>
2020-12-16 11:42:00 -0800

    typo fix

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -10,7 +10,7 @@ const TimelineEvent = (props) => {
         {props.person}
       </span>
       <span className="event-time">
-        <Timestamp time={props.timestamp} />
+        <Timestamp time={props.timeStamp} />
       </span>
       <p className="event-status">{props.status}</p>
     </div>
